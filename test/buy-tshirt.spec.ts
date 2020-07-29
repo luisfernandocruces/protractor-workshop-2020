9fc5db34843ce4e911e9f0bf71ea8b221434dc3a
luisVidal97 <luisfcv97@gmail.com>
2020-07-29 15:05:59 -0500

    delete time sleep

--- test/buy-tshirt.spec.ts
+++ test/buy-tshirt.spec.ts
@@ -25,7 +25,6 @@ describe('Buy a t-shirt', () => {
 
   it('then should be bought a t-shirt', async () => {
     await browser.get('http://automationpractice.com/');
-    await(browser.sleep(10000));
     await menuContentPage.goToTShirtMenu();
     await(browser.sleep(3000));
     await productListPage.goToModal();
