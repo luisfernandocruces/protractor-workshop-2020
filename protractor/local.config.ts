759e7209121afeccfe3b8b76f026ff0ad9e58b58
luisVidal97 <luisfcv97@gmail.com>
2020-07-29 16:56:39 -0500

    Implemented explicit wait

--- protractor/local.config.ts
+++ protractor/local.config.ts
@@ -9,7 +9,7 @@ export const config: Config = {
   onPrepare: () => {
     browser.ignoreSynchronization = true;
     reporter();
-    browser.manage().timeouts().implicitlyWait(3000);
+    // browser.manage().timeouts().implicitlyWait(3000);
   },
   jasmineNodeOpts: {
     defaultTimeoutInterval: 120000
